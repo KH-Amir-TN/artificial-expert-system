0257cdd8e54b56980764f33904eaf757816bf2e0
kh-amir-tn <amir.khemissi@insat.u-carthage.tn>
2021-10-20 20:32:30 +0100

    Adding logging.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import itertools
+import itertools,time
 
 #Constants
 FILES_ACCES_MODE = 'r'
@@ -7,6 +7,19 @@ PREM_CONJONCTION = " et "
 RULE_LBL_SEP = ':'
 RULE_PREFIX = "si"
 CON_SEP = " et "
+LOG_OUT = "./logs/"
+LOG_FILENAME = str( time.time() ) + ".log"
+
+LOG_FILE = open(LOG_OUT + LOG_FILENAME , "w")
+
+
+def log(msg):
+    if LOG_FILE:
+        LOG_FILE.writelines(str( msg ) + "\n")
+    print(msg)
+
+
+    
 
 def gen_list_of_combinations(items):
     res = []
@@ -21,11 +34,11 @@ def strip_list(items):
     return [ item.strip() for item in items]
 
 def print_list(items,msg):
-    print(msg)
+    log(msg)
     if len(items) == 0:
-        print("empty list.")
+        log("empty list.")
     for item in items:
-        print(item)
+        log(item)
 
 def form_rule(line):
     line = line.strip().lower()
@@ -56,65 +69,65 @@ def interfere(rules_db_filename,facts_db_filename,end_goal):
   rules = interfere_file(rules_db_filename,form_rule)
   facts = interfere_file(facts_db_filename,form_fact)
   
-  print(facts)
-  print(rules)
+  log(facts)
+  log(rules)
 
   while len(rules) > 0 :
-    print("-----------")
-    print("Facts in this iteration : ")
-    print(facts)
-    print("------------")
+    log("-----------")
+    log("Facts in this iteration : ")
+    log(facts)
+    log("------------")
 
     condidate_rules = []
     facts_combinitions_stages = gen_list_of_combinations(facts)
     new_facts = []
     for rule in rules:
-        print("-----------")
-        print("Rule to check : ")
-        print(rule)
-        print("------------")
+        log("-----------")
+        log("Rule to check : ")
+        log(rule)
+        log("------------")
         for stage in facts_combinitions_stages:
         
             for fact_combinition in stage:
-                print("------")
-                print("FACT COMBINATION: ")
-                print("------")
-                print(fact_combinition)
+                log("------")
+                log("FACT COMBINATION: ")
+                log("------")
+                log(fact_combinition)
                 if is_condidate(rule,fact_combinition):
                     condidate_rules.append(rule)
                     rules.remove(rule)
                     new_fact_rule =  condidate_rules[0]
                     new_fact = new_fact_rule["Conclusions"][0]
                     new_facts.append(condidate_rules[0]["Conclusions"][0])
-                    print("^^^^ SELECTED RULE IS CANDIDATE FOR FACT COMBINATION ^^^^")
-                    print("THE CANDIDATE RULES : ")
-                    print(condidate_rules)
-                    print("------")
-                    print("RULES : ")
-                    print(rules)
-                    print("------")
+                    log("^^^^ SELECTED RULE IS CANDIDATE FOR FACT COMBINATION ^^^^")
+                    log("THE CANDIDATE RULES : ")
+                    log(condidate_rules)
+                    log("------")
+                    log("RULES : ")
+                    log(rules)
+                    log("------")
                      #Check if goal is interfered?
                     if end_goal == new_fact  :
-                        print("####################")
-                        print("'" + end_goal + "'" + " has been interfered by rule : ")
-                        print(condidate_rules[0])
-                        print("RULES : ")
-                        print(rules)
-                        print("Facts : ")
-                        print(facts)
+                        log("####################")
+                        log("'" + end_goal + "'" + " has been interfered by rule : ")
+                        log(condidate_rules[0])
+                        log("RULES : ")
+                        log(rules)
+                        log("Facts : ")
+                        log(facts)
                         exit(0)
 
     if len(condidate_rules) == 0 :
-        print("#############")
-        print("[ERROR] : No candidate rule for the given facts :( ")
-        print("#####FACTS#######")
-        print(facts)
+        log("#############")
+        log("[ERROR] : No candidate rule for the given facts :( ")
+        log("#####FACTS#######")
+        log(facts)
         exit(1)
     
-    print("-----------")
-    print("Learnt facts in this iteration : ")
-    print(new_facts)
-    print("------------")
+    log("-----------")
+    log("Learnt facts in this iteration : ")
+    log(new_facts)
+    log("------------")
 
     facts = list( set( facts + new_facts ) ) 
     
@@ -138,4 +151,5 @@ def start():
 
 #Main program
 start()
+LOG_FILE.close()
 
