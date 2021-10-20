0ba5a02cba3b72cd80869470a4ca6575caae52cb
kh-amir-tn <amir.khemissi@insat.u-carthage.tn>
2021-10-20 16:01:07 +0100

    Refacto to improve printing and extending extraction capabilities.

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 
 #Constants
 FILES_ACCES_MODE = 'r'
-RULES_PREM_CONC_SEP = '>'
-PREM_CONJONCTION = '^'
+RULES_PREM_CONC_SEP = "alors"
+PREM_CONJONCTION = "et"
+RULE_LBL_SEP = ':'
+RULE_PREFIX = "si"
 
+
+def print_list(items,msg):
+    print(msg)
+    for item in items:
+        print(item)
 
 def form_rule(line):
     line = line.strip()
-    split = line.split(RULES_PREM_CONC_SEP)
+    split = line.split(RULE_LBL_SEP)
+    lbl = split[0]
+    rule = split[-1]
+    split = rule.split(RULE_PREFIX)
+    rule = split[-1]
+    split = rule.split(RULES_PREM_CONC_SEP)
     premises = split[0].split(PREM_CONJONCTION)
     conclusion = split[-1]
-    return [premises,conclusion]
+    return {lbl:{"Premises":premises,"Conclusion":conclusion}}
 
 def form_fact(line):
     return line.strip()
@@ -28,7 +40,7 @@ def interfere(rules_db_filename,facts_db_filename):
  
   rules = interfere_file(rules_db_filename,form_rule)
   facts = interfere_file(facts_db_filename,form_fact)
-  return [{"Rules":rules},{"Facts":facts}]
+  return [rules,facts]
 
    
 
@@ -36,8 +48,8 @@ def start():
   rules_db_filename = input("Please enter the filename of the rules database : ")
   facts_db_filename = input("Please enter the filename of the facts database : ")
   rules,facts=interfere(rules_db_filename,facts_db_filename)
-  print(rules)
-  print(facts)
+  print_list(rules,"Rules")
+  print_list(facts,"Facts")
 
 
 #Main program
