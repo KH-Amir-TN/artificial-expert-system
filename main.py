13222328f32a0d4f30b91294a0eb5d3d8ad941eb
kh-amir-tn <amir.khemissi@insat.u-carthage.tn>
2021-10-24 12:28:00 +0100

    Adding preflight checks to check if the given goal already exists in the initial list of facts. Refactoring and improving logging: Now logging to the console is enabled on purpose by setting the LOG_AND_PRINT to True otherwise the default behavior will be to log everything in a log file and only log to console when needed.

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ RULE_PREFIX = "si"
 CON_SEP = " et "
 LOG_OUT = "./logs/"
 LOG_FILENAME = str( time.time() ) + ".log"
+LOG_AND_PRINT = False 
 
 LOG_FILE = open(LOG_OUT + LOG_FILENAME , "w")
 
 
+def preflight_checks(facts,goal):
+    if goal in facts:
+        MSG = "The given goal '"+ goal +"' is a fact." 
+        log("#################",explicit_log_and_print=True)
+        log(MSG,explicit_log_and_print=True)
+        log("#############",explicit_log_and_print=True)
+        exit(0)
 
-def log(msg):
+def log(msg,explicit_log_and_print=False):
     if LOG_FILE:
         LOG_FILE.writelines(str( msg ) + "\n")
-    print(msg)
+    if LOG_AND_PRINT or explicit_log_and_print :
+        print(msg)
 
 def resolve_conflict(rules):
     return rules[0]
@@ -34,6 +43,14 @@ def is_candidate(rule,fact):
 
 def strip_list(items):
     return [ item.strip() for item in items]
+
+def print_completion(period):
+
+    PERIOD_PRECISION = '.3f'
+
+    log("##################################",explicit_log_and_print=True)
+    log("Completed in " + format(period,PERIOD_PRECISION) + " seconds." ,explicit_log_and_print=True)
+    log("##################################",explicit_log_and_print=True)
 
 def print_list(items,msg):
     log(msg)
@@ -70,14 +87,25 @@ def interfere(rules_db_filename,facts_db_filename,end_goal):
  
   init_rules = interfere_file(rules_db_filename,form_rule)
   init_facts = interfere_file(facts_db_filename,form_fact)
+
+  log("######### INIT FACTS ##############",explicit_log_and_print=True)
+  log(init_facts,explicit_log_and_print=True)
+  log("######### INIT RULES ##############",explicit_log_and_print=True)
+  log(init_rules,explicit_log_and_print=True)
+  log("####################################",explicit_log_and_print=True)
+
   facts = init_facts
   rules = init_rules
-  log(facts)
-  log(rules)
 
+  
+  preflight_checks(facts,end_goal)
+  
+  start_time = time.time()
+  if not LOG_AND_PRINT:
+      print("Interfering ...")
 
   while len(rules) > 0 :
-
+    
     log("#####")
     log("NEW ITERATION")
     log("#####")
@@ -129,24 +157,29 @@ def interfere(rules_db_filename,facts_db_filename,end_goal):
                 rules.remove(new_fact_rule)
                 #Check if goal is interfered?
                 if end_goal in new_fact  :
-                    log("####################")
-                    log("'" + end_goal + "'" + " has been interfered by rule : ")
-                    log(new_fact_rule)
-                    log("RULES : ")
-                    log(rules)
-                    log("Facts : ")
-                    log(facts)
+                    end_time = time.time()
+                    period = end_time - start_time
+                    log("####################",explicit_log_and_print=True)
+                    log("'" + end_goal + "'" + " has been interfered by rule : ",explicit_log_and_print=True)
+                    log(new_fact_rule,explicit_log_and_print=True)
+                    log("RULES : ",explicit_log_and_print=True)
+                    log(rules,explicit_log_and_print=True)
+                    log("Facts : ",explicit_log_and_print=True)
+                    log(facts,explicit_log_and_print=True)
+                    print_completion(period)
                     exit(0)
 
     if len(rules) == init_rules_len :
+        end_time = time.time()
+        period = end_time - start_time
+        log("#############",explicit_log_and_print=True)
+        log("[ERROR] : No candidate rule for the given facts :( ",explicit_log_and_print=True)
+        log("#####FACTS#######",explicit_log_and_print=True)
+        log(facts,explicit_log_and_print=True)
+        log("#####RULES#######",explicit_log_and_print=True)
+        log(rules,explicit_log_and_print=True)
 
-        log("#############")
-        log("[ERROR] : No candidate rule for the given facts :( ")
-        log("#####FACTS#######")
-        log(facts)
-        log("#####RULES#######")
-        log(rules)
-        
+        print_completion(period)
         exit(1)
                   
     log("-----------")
@@ -156,10 +189,9 @@ def interfere(rules_db_filename,facts_db_filename,end_goal):
     facts = list( set( facts + new_facts ) ) 
 
     
-    
+  end_time = time.time()
 
-
-  return [rules,facts,candidate_rules]
+  return [rules,facts,end_time - start_time]
 
    
 
@@ -169,14 +201,14 @@ def start():
   end_goal = input("Please enter what you want to infere? ").lower().strip()
   if len(end_goal) == 0:
       print("You did not provide an intended goal to interfere,therefore the program will interfere until it reaches a dead end.")
- 
-  rules,facts,candidate_rules = interfere(rules_db_filename,facts_db_filename,end_goal)
-  log("-----")
+  
+  rules,facts,period = interfere(rules_db_filename,facts_db_filename,end_goal)
+  log("--- END RULES ---")
   print_list(rules,"Rules")
-  log("-----")
+  log("--- END FACTS ---")
   print_list(facts,"Facts")
-  log("-----")
-  print_list(candidate_rules,"Candidate rules")   
+
+  print_completion(period)
 
 #Main program
 start()
