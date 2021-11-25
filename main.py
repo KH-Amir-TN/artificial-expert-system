9237e1f0ff1478d14ae016cba249f47fbeae723f
kh-amir-tn <amir.khemissi@insat.u-carthage.tn>
2021-11-25 11:36:30 +0100

    Required the input of the end goal.

--- main.py
+++ main.py
@@ -104,89 +104,7 @@ def interfere(rules_db_filename,facts_db_filename,end_goal):
   if not LOG_AND_PRINT:
       print("Interfering ...")
 
-  while len(rules) > 0 :
-    
-    log("#####")
-    log("NEW ITERATION")
-    log("#####")
-
-    log("-----------")
-    log("Facts in this iteration : ")
-    log(facts)
-    log("------------")
-
-    log("------")
-    log("RULES : ")
-    log(rules)
-    log("------")
-
- 
-
-    facts_combinitions_stages = gen_list_of_combinations(facts)
-
-
-    new_facts = []
-    init_rules_len = len(rules)
-    for stage in facts_combinitions_stages:
-        
-        for fact_combinition in stage:
-            log("------")
-            log("FACT COMBINATION: ")
-            log("------")
-            log(fact_combinition)
-            candidate_rules = []
-            for rule in rules:
-                log("-----------")
-                log("Rule to check : ")
-                log(rule)
-                log("------------")
-
-                if is_candidate(rule,fact_combinition):
-                    candidate_rules.append(rule)
-                    log("---------")
-                    log("^^^^ SELECTED RULE IS CANDIDATE FOR FACT COMBINATION ^^^^")
-                    log("THE CANDIDATE RULES BECOMES : ")
-                    log(candidate_rules)
-                    log("---------")
-
-
-            if  len(candidate_rules) > 0 :
-                new_fact_rule = resolve_conflict(candidate_rules)
-                new_fact = new_fact_rule["Conclusions"]
-                new_facts += new_fact
-                rules.remove(new_fact_rule)
-                #Check if goal is interfered?
-                if end_goal in new_fact  :
-                    end_time = time.time()
-                    period = end_time - start_time
-                    log("####################",explicit_log_and_print=True)
-                    log("'" + end_goal + "'" + " has been interfered by rule : ",explicit_log_and_print=True)
-                    log(new_fact_rule,explicit_log_and_print=True)
-                    log("RULES : ",explicit_log_and_print=True)
-                    log(rules,explicit_log_and_print=True)
-                    log("Facts : ",explicit_log_and_print=True)
-                    log(facts,explicit_log_and_print=True)
-                    print_completion(period)
-                    exit(0)
-
-    if len(rules) == init_rules_len :
-        end_time = time.time()
-        period = end_time - start_time
-        log("#############",explicit_log_and_print=True)
-        log("[ERROR] : No candidate rule for the given facts :( ",explicit_log_and_print=True)
-        log("#####FACTS#######",explicit_log_and_print=True)
-        log(facts,explicit_log_and_print=True)
-        log("#####RULES#######",explicit_log_and_print=True)
-        log(rules,explicit_log_and_print=True)
-
-        print_completion(period)
-        exit(1)
-                  
-    log("-----------")
-    log("Learnt facts in this iteration : ")
-    log(new_facts)
-    log("------------")
-    facts = list( set( facts + new_facts ) ) 
+  
 
     
   end_time = time.time()
@@ -199,8 +117,10 @@ def start():
   rules_db_filename = input("Please enter the filename of the rules database : ")
   facts_db_filename = input("Please enter the filename of the facts database : ")
   end_goal = input("Please enter what you want to infere? ").lower().strip()
-  if len(end_goal) == 0:
-      print("You did not provide an intended goal to interfere,therefore the program will interfere until it reaches a dead end.")
+  while(len(end_goal) == 0):
+   if len(end_goal) == 0:
+      print("You did not provide an intended goal to interfere!")
+   end_goal = input("Please enter what you want to infere? ").lower().strip()
   
   rules,facts,period = interfere(rules_db_filename,facts_db_filename,end_goal)
   log("--- END RULES ---")
